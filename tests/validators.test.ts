01448bedc315bd03929503cfb883a00edb98b4ee
masykuri syaifullah <msyaifullah@gmail.com>
2021-11-19 12:21:44 +0800

    feat: added validator sample modules and testing

---
+++ tests/validators.test.ts
@@ -0,0 +1,17 @@
+/**
+ * Create by Masykuri Syaifullah 20/12/2021
+ * Validate by Masykuri Syaifullah 21/12/2021
+ */
+import { ZipCodeValidator } from "../src/Classes/Validator/ZipCodeValidator"; // this will be your custom import
+import { expect } from "chai";
+
+describe('ZipCodeValidator tests', () => { // the tests container
+
+    it('checking default ZipCodeValidator', () => { // the single test
+        const zipCodeValidator = new ZipCodeValidator(); // this will be your class
+
+        expect(zipCodeValidator.isAcceptable('123456')).to.be.false;
+
+    });
+
+});
